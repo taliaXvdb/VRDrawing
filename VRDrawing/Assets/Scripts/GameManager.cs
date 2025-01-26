bc031ad56a1b0e3fb8daba6a0f698ea005a11468
taliaXvdb <talia.van.den.bossche@student.howest.be>
2025-01-26 18:58:47 +0100

    Feat21: welcome in main

--- VRDrawing/Assets/Scripts/GameManager.cs
+++ VRDrawing/Assets/Scripts/GameManager.cs
@@ -8,29 +8,63 @@ using Flexalon;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject mushroomPrefab;
+    [SerializeField] private Canvas _welcomeCanvas;
     [SerializeField] private Canvas _startTutorialCanvas;
     [SerializeField] private List<Button> _buttons;
     private TMP_Text _tutorialText;
+    private TextTyper _textTyper;
     public bool isTutorial = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        _textTyper = GameObject.FindObjectOfType<TextTyper>();
+
+        if (!_welcomeCanvas.gameObject.activeSelf)
+        {
+            _welcomeCanvas.gameObject.SetActive(true);
+            _tutorialText = _welcomeCanvas.GetComponentInChildren<TMP_Text>();
+
+            if (_tutorialText.CompareTag("FullText"))
+            {
+                _textTyper.StartTyping(_tutorialText);
+            }
+            StartCoroutine(ShowTutorial());
+        }
+    }
+
+    private IEnumerator ShowTutorial()
+    {
+        // Wait until the typing is complete
+        while (_textTyper.isTyping)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2f);
+        // After typing finishes, hide the welcome canvas
+        _welcomeCanvas.gameObject.SetActive(false);
+
+        // Now activate the tutorial canvas
         TextWriter textWriter = GameObject.FindObjectOfType<TextWriter>();
         textWriter.enabled = true;
         textWriter.canvas = _startTutorialCanvas;
         textWriter.buttons = _buttons;
+
         if (_startTutorialCanvas.GetComponentInChildren<TMP_Text>().CompareTag("FullText"))
         {
             _tutorialText = _startTutorialCanvas.GetComponentInChildren<TMP_Text>();
             textWriter.uiText = _tutorialText;
         }
 
+        // Show the start tutorial canvas
+        _startTutorialCanvas.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!isTutorial)
+        if (!isTutorial)
         {
             _startTutorialCanvas.gameObject.SetActive(false);
         }
